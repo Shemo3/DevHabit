69ef0576df3c538cdf6d842efa48690fa6cdc40b
She-MO <nikitashp3@gmail.com>
2025-05-29 21:14:10 +0300

    Add OpenTelemetry in Program.cs

--- DevHabit.Api/Program.cs
+++ DevHabit.Api/Program.cs
@@ -1,3 +1,8 @@
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
 namespace DevHabit.Api;
 
 public static class Program
@@ -8,6 +13,23 @@ public static class Program
 
         builder.Services.AddControllers();
         builder.Services.AddOpenApi();
+
+        builder.Services.AddOpenTelemetry()
+            .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName))
+            .WithTracing(tracing => tracing
+                .AddHttpClientInstrumentation()
+                .AddAspNetCoreInstrumentation())
+            .WithMetrics(metrics => metrics
+                .AddHttpClientInstrumentation()
+                .AddAspNetCoreInstrumentation()
+                .AddRuntimeInstrumentation())
+            .UseOtlpExporter();
+
+        builder.Logging.AddOpenTelemetry(options =>
+        {
+            options.IncludeScopes = true;
+            options.IncludeFormattedMessage = true;
+        });
 
         WebApplication app = builder.Build();
 
