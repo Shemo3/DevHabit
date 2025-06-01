3df3ac91afa9e9ca9f19ae9642e8b251f78a7565
She-MO <nikitashp3@gmail.com>
2025-06-01 16:05:29 +0300

    Add habits db set to db context

--- DevHabit/DevHabit.Api/Database/ApplicationDbContext.cs
+++ DevHabit/DevHabit.Api/Database/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DevHabit.Api.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevHabit.Api.Database;
 
 public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
 {
+    public DbSet<Habit> Habits { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema(Schemas.Application);
