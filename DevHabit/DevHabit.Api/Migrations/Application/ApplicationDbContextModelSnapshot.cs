ae5f323c9be1d6be707a958cf41ee6d96eeb3fef
She-MO <nikitashp3@gmail.com>
2025-06-05 16:12:28 +0300

    Replace previous migration "Add_HabitTags" with a new one, which contains all db context changes

--- DevHabit/DevHabit.Api/Migrations/Application/ApplicationDbContextModelSnapshot.cs
+++ DevHabit/DevHabit.Api/Migrations/Application/ApplicationDbContextModelSnapshot.cs
@@ -75,6 +75,65 @@ namespace DevHabit.Api.Migrations.Application
                     b.ToTable("habits", "dev_habit");
                 });
 
+            modelBuilder.Entity("DevHabit.Api.Entities.HabitTag", b =>
+                {
+                    b.Property<string>("HabitId")
+                        .HasColumnType("character varying(500)")
+                        .HasColumnName("habit_id");
+
+                    b.Property<string>("TagId")
+                        .HasColumnType("character varying(500)")
+                        .HasColumnName("tag_id");
+
+                    b.Property<DateTime>("CreatedAtUtc")
+                        .HasColumnType("timestamp with time zone")
+                        .HasColumnName("created_at_utc");
+
+                    b.HasKey("HabitId", "TagId")
+                        .HasName("pk_habit_tags");
+
+                    b.HasIndex("TagId")
+                        .HasDatabaseName("ix_habit_tags_tag_id");
+
+                    b.ToTable("habit_tags", "dev_habit");
+                });
+
+            modelBuilder.Entity("DevHabit.Api.Entities.Tag", b =>
+                {
+                    b.Property<string>("Id")
+                        .HasMaxLength(500)
+                        .HasColumnType("character varying(500)")
+                        .HasColumnName("id");
+
+                    b.Property<DateTime>("CreatedAtUtc")
+                        .HasColumnType("timestamp with time zone")
+                        .HasColumnName("created_at_utc");
+
+                    b.Property<string>("Description")
+                        .HasMaxLength(500)
+                        .HasColumnType("character varying(500)")
+                        .HasColumnName("description");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(50)
+                        .HasColumnType("character varying(50)")
+                        .HasColumnName("name");
+
+                    b.Property<DateTime?>("UpdatedAtUtc")
+                        .HasColumnType("timestamp with time zone")
+                        .HasColumnName("updated_at_utc");
+
+                    b.HasKey("Id")
+                        .HasName("pk_tags");
+
+                    b.HasIndex("Name")
+                        .IsUnique()
+                        .HasDatabaseName("ix_tags_name");
+
+                    b.ToTable("tags", "dev_habit");
+                });
+
             modelBuilder.Entity("DevHabit.Api.Entities.Habit", b =>
                 {
                     b.OwnsOne("DevHabit.Api.Entities.Frequency", "Frequency", b1 =>
@@ -155,6 +214,28 @@ namespace DevHabit.Api.Migrations.Application
 
                     b.Navigation("Target")
                         .IsRequired();
+                });
+
+            modelBuilder.Entity("DevHabit.Api.Entities.HabitTag", b =>
+                {
+                    b.HasOne("DevHabit.Api.Entities.Habit", null)
+                        .WithMany("HabitTags")
+                        .HasForeignKey("HabitId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired()
+                        .HasConstraintName("fk_habit_tags_habits_habit_id");
+
+                    b.HasOne("DevHabit.Api.Entities.Tag", null)
+                        .WithMany()
+                        .HasForeignKey("TagId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired()
+                        .HasConstraintName("fk_habit_tags_tags_tag_id");
+                });
+
+            modelBuilder.Entity("DevHabit.Api.Entities.Habit", b =>
+                {
+                    b.Navigation("HabitTags");
                 });
 #pragma warning restore 612, 618
         }
