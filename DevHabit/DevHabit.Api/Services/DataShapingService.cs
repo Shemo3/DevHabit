9f471844e47c9c8a2ae3ef1a1b3c6374f50c8298
She-MO <nikitashp3@gmail.com>
2025-06-08 19:37:05 +0300

    Add HATEOAS support to ShapeCollectionData method of DataShapingService

--- DevHabit/DevHabit.Api/Services/DataShapingService.cs
+++ DevHabit/DevHabit.Api/Services/DataShapingService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Concurrent;
 using System.Dynamic;
 using System.Reflection;
+using DevHabit.Api.DTOs.Common;
 
 namespace DevHabit.Api.Services;
 
@@ -32,7 +33,9 @@ public sealed class DataShapingService
         return (ExpandoObject)shapedObject;
     }
 
-    public List<ExpandoObject> ShapeCollectionData<T>(IEnumerable<T> entities, string? fields)
+    public List<ExpandoObject> ShapeCollectionData<T>(IEnumerable<T> entities,
+        string? fields,
+        Func<T, List<LinkDto>>? linksFactory = null)
     {
         var fieldsSet = fields?
             .Split(',', StringSplitOptions.RemoveEmptyEntries)
@@ -54,6 +57,11 @@ public sealed class DataShapingService
             foreach (PropertyInfo propertyInfo in propertyInfos)
             {
                 shapedObject[propertyInfo.Name] = propertyInfo.GetValue(entity);
+            }
+
+            if (linksFactory is not null)
+            {
+                shapedObject["links"] = linksFactory(entity);
             }
 
             shapedObjects.Add((ExpandoObject)shapedObject);
