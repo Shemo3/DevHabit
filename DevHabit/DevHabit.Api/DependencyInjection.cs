1b050f679a03cbbe10f97fcb4156b0ddfdc69f0b
She-MO <nikitashp3@gmail.com>
2025-06-11 17:47:59 +0300

    Add support for versioning based on custom media type

--- DevHabit/DevHabit.Api/DependencyInjection.cs
+++ DevHabit/DevHabit.Api/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using DevHabit.Api.Database;
+﻿using Asp.Versioning;
+using DevHabit.Api.Database;
 using DevHabit.Api.DTOs.Habits;
 using DevHabit.Api.Entities;
 using DevHabit.Api.Middleware;
@@ -34,8 +35,25 @@ public static class DependencyInjection
             var formatter = options.OutputFormatters
                 .OfType<NewtonsoftJsonOutputFormatter>()
                 .First();
+            formatter.SupportedMediaTypes.Add(CustomMediaTypeNames.Application.JsonV1);
+            formatter.SupportedMediaTypes.Add(CustomMediaTypeNames.Application.JsonV2);
             formatter.SupportedMediaTypes.Add(CustomMediaTypeNames.Application.HateoasJson);
+            formatter.SupportedMediaTypes.Add(CustomMediaTypeNames.Application.HateoasJsonV1);
+            formatter.SupportedMediaTypes.Add(CustomMediaTypeNames.Application.HateoasJsonV2);
         });
+
+        builder.Services.AddApiVersioning(options =>
+        {
+            options.DefaultApiVersion = new ApiVersion(1.0);
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.ReportApiVersions = true;
+            options.ApiVersionSelector = new DefaultApiVersionSelector(options);
+            options.ApiVersionReader = ApiVersionReader.Combine(
+                new MediaTypeApiVersionReader(),
+                new MediaTypeApiVersionReaderBuilder()
+                    .Template("application/vnd.dev-habit.hateoas.{version}+json")
+                    .Build());
+        }).AddMvc();
         builder.Services.AddOpenApi();
 
         return builder;
